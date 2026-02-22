41d51e5d8b3e592a7fe89c31aa10cb367ad2f05d
Developer <developer@simbaexpress.com>
2026-02-22 14:17:09 +0300

    Fix monorepo setup: clean up tsconfig, fix client dependencies, add CSS types, enable proper ESLint configuration

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,9 +6,9 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "node_modules", "../server"]),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["src/**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
